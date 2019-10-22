0568bd32a73d16b61570dc063f9c9581da06c74f
Rahul Pandita <rahul.p@lendenclub.com>
2019-10-23 01:57:19 +0530

    working multi replace code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+    state = {
+        inputText: '',
+        resultText: '',
+        findText: '',
+        replaceText: ''
+    }
+    
+    handleInputText = (e) => {
+        this.setState({ inputText: e.target.value })
+    }
+    
+    handleFindText = (e) => {
+        this.setState({ findText: e.target.value })
+    }
+    
+    handleReplaceText = (e) => {
+        this.setState({ replaceText: e.target.value })
+    }
+    
+    replace = () => {
+        let { inputText, resultText, findText, replaceText } = this.state;
+        
+        let keyPairs = findText.split(',');
+        let valuePairs = replaceText.split(',');
+        
+        let theDict = {};
+        
+        for(var i = 0; i < keyPairs.length; i++) {
+            theDict[keyPairs[i]] = valuePairs[i];
+        }
+        
+        let regexString = keyPairs.join('|');
+        let re = new RegExp(regexString, 'gim');
+        resultText = inputText.replace(re, (match) => theDict[match]);
+        
+        this.setState({ resultText })
+    }
+    
+    render () {
+        
+        let { inputText, resultText, findText, replaceText } = this.state;
+        
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              
+              <textarea 
+                rows={10} 
+                cols={50}
+                style={{
+                    borderRadius: 4,
+                }}
+                value={inputText}
+                onChange={this.handleInputText}
+              >
+                
+              </textarea>
+              
+              
+              <div
+                style={{
+                    fontSize: 12,
+                    display: 'flex',
+                    alignItems: 'center'
+                }}
+              >
+                  <label> Find: </label>
+                  <input  
+                    value={findText}
+                    onChange={this.handleFindText}
+                    style={{
+                        margin: 5,
+                        borderRadius: 4
+                    }}
+                  />
+              </div>
+              
+             
+              <div
+                style={{
+                    fontSize: 12,
+                    display: 'flex',
+                    alignItems: 'center'
+                }}
+              >
+                  <label> Replace: </label>
+                  <input  
+                    value={replaceText}
+                    onChange={this.handleReplaceText}
+                    style={{
+                        margin: 5,
+                        borderRadius: 4
+                    }}
+                  />
+              </div>
+              
+              
+              <button
+                onClick={this.replace}
+                style={{
+                    margin: 5
+                }}
+              >
+                Replace
+              </button>
+              
+              <textarea 
+                rows={10} 
+                cols={50}
+                style={{
+                    borderRadius: 4,
+                }}
+                value={resultText}
+              >
+                
+              </textarea>
+              
+            </header>
+            
+          </div>
+        );
+    }
 }
 
 export default App;
